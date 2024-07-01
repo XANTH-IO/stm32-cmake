fc12d577cbaaf527db57ec53be30cbb1f88dd846
Maxime JOURDAN - XANTHIO <contact@xanth.io>
2024-07-01 17:49:40 +0200

    Prepare WL33 subfamily support

--- cmake/stm32/wl.cmake
+++ cmake/stm32/wl.cmake
@@ -1,11 +1,13 @@
+# Note: Can't support STM32WL33 yet because cmsis doesn't provide startup
+
 set(STM32_WL_TYPES
-    WL33xx WL33xx
-    WL54xx WL55xx WL55xx
+#    WL33xx WL33xx
+    WL54xx WL55xx WL5Mxx
     WLE4xx WLE4xx WLE4xx WLE5xx WLE5xx WLE5xx
 )
 
 set(STM32_WL_TYPE_MATCH 
-    "WL33.8" "WL33.[BC]"
+#    "WL33.8" "WL33.[BC]"
     "WL54.." "WL55.." "WL5MOC"
     "WLE4.8" "WLE4.B" "WLE4.C" "WLE5.8" "WLE5.B" "WLE5.C"
 )
@@ -15,20 +17,20 @@ set(STM32_WL_TYPE_MATCH
 # there are 2 split sections of RAM and our default linker script only manages 
 # one section.
 set(STM32_WL_M4_RAM_SIZES 
-     0K  0K
+#     0K  0K
     32K 32K 32K
     10K 24K 64K 10K 24K 64K
 )
 
 # this is RAM size allocated to M0PLUS core
 set(STM32_WL_M0PLUS_RAM_SIZES 
-    16K 32K
+#    16K 32K
     32K 32K 32K
      0K  0K  0K  0K  0K  0K
 )
 
 set(STM32_WL_CCRAM_SIZES 
-    0K 0K
+    # 0K 0K
     0K 0K 0K
     0K 0K 0K 0K 0K 0K
 )
@@ -56,18 +58,39 @@ function(stm32wl_get_memory_info DEVICE TYPE CORE RAM FLASH_ORIGIN RAM_ORIGIN TW
     list(GET STM32_WL_M0PLUS_RAM_SIZES ${TYPE_INDEX} RAM_M0PLUS_VALUE)
     list(GET STM32_WL_M4_RAM_SIZES ${TYPE_INDEX} RAM_M4_VALUE)
     if(NOT (RAM_M0PLUS_VALUE EQUAL 0K) AND NOT (RAM_M4_VALUE EQUAL 0K))
+        # dual core (WL5X)
         set(${TWO_FLASH_BANKS} TRUE PARENT_SCOPE)
-    else()
+        if(CORE STREQUAL "M4")
+            set(${RAM} ${RAM_M4_VALUE} PARENT_SCOPE)
+            set(${FLASH_ORIGIN} 0x08000000 PARENT_SCOPE)
+            set(${RAM_ORIGIN} 0x20000000 PARENT_SCOPE)            
+        elseif(CORE STREQUAL "M0PLUS")
+            set(${RAM} ${RAM_M0PLUS_VALUE} PARENT_SCOPE)
+            set(${FLASH_ORIGIN} 0x08020000 PARENT_SCOPE)
+            set(${RAM_ORIGIN} 0x20008000 PARENT_SCOPE)
+        else()
+            message(FATAL_ERROR "Unknown core ${CORE}")
+        endif()
+    elseif(NOT (RAM_M0PLUS_VALUE EQUAL 0K) AND (RAM_M4_VALUE EQUAL 0K))
+        # single M0+ core (WL33)
         set(${TWO_FLASH_BANKS} FALSE PARENT_SCOPE)
-    endif()
-    if((CORE STREQUAL "M4") AND NOT (RAM_M4_VALUE EQUAL 0K))
-        set(${RAM} ${RAM_M4_VALUE} PARENT_SCOPE)
-        set(${FLASH_ORIGIN} 0x8000000 PARENT_SCOPE)
-        set(${RAM_ORIGIN} 0x20000000 PARENT_SCOPE)
-    elseif((CORE STREQUAL "M0PLUS") AND NOT (RAM_M0PLUS_VALUE EQUAL 0K))
-        set(${RAM} ${RAM_M0PLUS_VALUE} PARENT_SCOPE)
-        set(${FLASH_ORIGIN} 0x8020000 PARENT_SCOPE)
-        set(${RAM_ORIGIN} 0x20008000 PARENT_SCOPE)
+        if(CORE STREQUAL "M0PLUS")
+            set(${RAM} ${RAM_M0PLUS_VALUE} PARENT_SCOPE)
+            set(${FLASH_ORIGIN} 0x10040000 PARENT_SCOPE)
+            set(${RAM_ORIGIN} 0x20000000 PARENT_SCOPE)
+        else()
+            message(FATAL_ERROR "Type ${TYPE} has no core ${CORE}")
+        endif()
+    elseif((RAM_M0PLUS_VALUE EQUAL 0K) AND NOT (RAM_M4_VALUE EQUAL 0K))
+        # single M4 core (WLEX)
+        set(${TWO_FLASH_BANKS} FALSE PARENT_SCOPE)
+        if(CORE STREQUAL "M4")
+            set(${RAM} ${RAM_M4_VALUE} PARENT_SCOPE)
+            set(${FLASH_ORIGIN} 0x08000000 PARENT_SCOPE)
+            set(${RAM_ORIGIN} 0x20000000 PARENT_SCOPE)
+        else()
+            message(FATAL_ERROR "Type ${TYPE} has no core ${CORE}")
+        endif()
     else()
         message(FATAL_ERROR "Unknown core ${CORE}")
     endif()
@@ -87,12 +110,12 @@ function(stm32wl_get_device_cores DEVICE TYPE CORES)
 endfunction()
 
 list(APPEND STM32_ALL_DEVICES
-    WL33C8
-    WL33CB
-    WL33CC
-    WL33K8
-    WL33KB
-    WL33KC
+    # WL33C8
+    # WL33CB
+    # WL33CC
+    # WL33K8
+    # WL33KB
+    # WL33KC
     WL54CC
     WL54JC
     WL55CC
