c634e861e31714bc08bd46a979a73ae05a8ab9da
Maxime JOURDAN - XANTHIO <contact@xanth.io>
2024-09-19 11:55:02 +0200

    Adding tests for Freertos

---
+++ tests/freertos/main.c
@@ -0,0 +1,23 @@
+#include <FreeRTOS.h>
+#include <task.h>
+
+static void task(void *arg)
+{
+    for(;;)
+    {
+        vTaskDelay(500);
+    }
+}
+
+int main(void)
+{
+    SystemInit();
+    blinky::init();
+    
+    xTaskCreate(task, "blinky", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
+    
+    vTaskStartScheduler();
+    for (;;);
+    
+    return 0;
+}
