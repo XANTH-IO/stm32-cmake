6c01b22124d02b08f065327430ed2d4ce70db7d6
Maxime JOURDAN - XANTHIO <contact@xanth.io>
2024-12-24 14:25:04 +0100

    WIP as WBA6x are not yet supported by CMSIS

--- cmake/stm32/wba.cmake
+++ cmake/stm32/wba.cmake
@@ -1,20 +1,24 @@
 set(STM32_WBA_TYPES
     WBA50xx
     WBA52xx WBA54xx WBA55xx
+    WBA62xx WBA63xx WBA64xx WBA65xx
 )
 
 set(STM32_WBA_TYPE_MATCH
     "WBA50KG"
     "WBA52.[EG]" "WBA54.[EG]" "WBA55.[EG]"
+    "WBA62.[GI]" "WBA63.[GI]" "WBA64.[GI]" "WBA65.[GI]"
 )
 
 set(STM32_WBA_RAM_SIZES 
     0K
     0K 0K 0K
+    0K 0K 0K 0K
 )
 set(STM32_WBA_CCRAM_SIZES 
     0K
     0K 0K 0K
+    0K 0K 0K 0K
 )
 
 stm32_util_create_family_targets(WBA)
@@ -31,14 +35,21 @@ function(stm32wba_get_memory_info DEVICE TYPE FLASH_SIZE RAM_SIZE)
 
     if (SUB STREQUAL "50")
         set(RAM "64K")
-    else()
+    elseif(SUB STREQUAL "52" OR SUB STREQUAL "54" OR SUB STREQUAL "55")
         if(F STREQUAL "E")
             set(RAM "96K")
         elseif(F STREQUAL "G")
             set(RAM "128K")
         else()
-            message(FATAL_ERROR "Unable to get RAM size for ${DEVICE}.")
         endif()
+    elseif(SUB STREQUAL "62" OR SUB STREQUAL "63" OR SUB STREQUAL "64" OR SUB STREQUAL "65")
+        if(F STREQUAL "G")
+            set(RAM "256K")
+        elseif(F STREQUAL "I")
+            set(RAM "512K")
+        endif()
+    else()
+        message(FATAL_ERROR "Unable to get RAM size for ${DEVICE}.")
     endif()
     
     set(${RAM_SIZE} ${RAM} PARENT_SCOPE)
@@ -59,6 +70,23 @@ set(STM32_WBA_DEVICES
     WBA55HG
     WBA55UE
     WBA55UG
+    WBA62CG
+    WBA62CI
+    WBA62MG
+    WBA62MI
+    WBA62PG
+    WBA63CG
+    WBA63CI
+    WBA64CG
+    WBA64CI
+    WBA65CG
+    WBA65CI
+    WBA65MG
+    WBA65MI
+    WBA65PG
+    WBA65PI
+    WBA65RG
+    WBA65RI
 )
 list(APPEND STM32_ALL_DEVICES STM32_WBA_DEVICES)
 
